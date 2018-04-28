6c6a11554d7070a5eb5d38ead8cbdef05b835fd0
Hoang Nguyen <phuonghoang713@gmail.com>
2018-04-28 17:16:02 +0700

    debug order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,17 @@
 class OrdersController < ApplicationController
+  
+  def new
+    @item = FoodItem.find(params[:food_item_id])
+  end
+
+  def create 
+    @item = FoodItem.find(params[:food_item_id])
+    @order = @item.orders.build order_params
+    raise "hello"
+  end
+
+  def order_params
+    params.require(:order).permit(:quantity)
+  end
+
 end
--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -1,7 +1,8 @@
 class FoodItem < ApplicationRecord
   belongs_to :section
   validates :name, :price, presence: true
-
+  has_many :orders
+  
   def image_url_or_default
     (!image_url.nil? && !image_url.blank?)? image_url : "http://toinayangi.vn/wp-content/uploads/2017/09/cach-lam-com-ga-tay-cam-1.jpg"
   end
